30de9124e6bd7e1608420df1629d977650fb3f5a
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-09-12 00:39:00 -0800

    Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import './Components/style.css';
+// import './Components/style.css';
 // import StateUse from './state';
 // import JSX from "./JSX";
 // import Event from "./Event";
 // import ClassUsingState from './Components/ClassUsingState';
+// import Prop from './Components/Props/Prop';
+// import './Components/Props/prop.css';
+
+import OnChange from './Components/GetDataOnChange/OnChange';
 
 function App() {
   return (
@@ -13,6 +17,12 @@ function App() {
        {/* <Event /> */}
        {/* <StateUse /> */}
       {/* <ClassUsingState /> */}
+      {/* props */}
+      {/* <Prop name="Prince" email="prince@gmail.com" a/> */}
+      {/* <Prop name="Rahul Sharma" email="rahul@gmail.com"/>
+      <Prop name="Aman Sharma" email="aman@gmail.com"/> */}
+
+      <OnChange />
     </div>
   );
 }
--- src/Components/Props/Prop.js
+++ src/Components/Props/Prop.js
@@ -1,8 +1,15 @@
-function Student()
+import React, {useState} from 'react';
+
+function Student(props)
 {
+    const [name, setName] = useState("Prince");
+    // console.log(props.name);
     return(
         <div>
-            <h1>Hello Prince</h1>
+            <h1>Hello {name}</h1>
+            {/* <h2>Email : {props.email}</h2> */}
+            <button onClick={() =>{setName("Rahul")}}>Update Data</button>
+             
         </div>
     )
 }
